44cddbc4460773f144f395d0109b80c89f483589
ford0318 <ford0318@gmail.com>
2020-02-13 15:29:05 +0800

    first

---
+++ home/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+urlpatterns = [
+    path('',views.index),
+    path('gallery/',views.gallery),
+    path('styletransfer/',views.styletransfer),
+    path('styletransfer2/',views.styletransfer2),
+    path('delmypic/',views.delmypic,name='uplaodfile'),
+    # path('styletransfer/{imgpath:imgpath}',views.styletransfer),
+    path('result/',views.result),
+    path('objectdetection/',views.objectdetection),
+    path('facerecognition/',views.facerecognition),
+]
---
+++ home/views.py
@@ -0,0 +1,93 @@
+from django.shortcuts import render,redirect
+from django.http import HttpResponse
+from django.core.files.storage import FileSystemStorage
+from .dlib import forImport_recognize_faces_image
+from .BeautyGAN import main as BeautyGAN
+from .BeautyGAN import split as beautysplit
+import cv2
+import glob
+import os
+# import subprocess
+import tensorflow as tf
+
+# from .flask.peeweetest import Classified
+
+# from .object_detection.evaluate import YoloTest
+# YoloTest = YoloTest()
+
+# Create your views here.
+def index(request):
+
+    #return HttpResponse("<p>Hello world!</p>")
+    return render(request,'index.html')
+    
+def gallery(request):
+    return render(request,'gallery.html')
+
+def facerecognition(request):
+    if request.method =='POST' and request.FILES['photoupload']:
+        myfile=request.FILES['photoupload']
+        fs = FileSystemStorage(location='home/static/images/')
+        fs.save(myfile.name,myfile)
+        forImport_recognize_faces_image.readPara("home/dlib/encoding3.pickle",f'home/static/images/{myfile.name}','cnn')
+        photopath="images/upload.jpg"
+    title="FACE RECOGNITION"
+    return render(request,'layout.html',locals())
+
+
+def styletransfer(request):  
+    if request.method =='POST' and request.FILES['photoupload']:
+        myfile=request.FILES['photoupload']
+        fs = FileSystemStorage(location='home/static/images/')
+        fs.save(myfile.name,myfile)
+        # forImport_recognize_faces_image.readPara("home/dlib/encoding3.pickle",f'home/static/images/{myfile.name}','cnn')
+        BeautyGAN.beauty(f'home/static/images/{myfile.name}')
+        makeups = glob.glob(os.path.join('home','static','makeupstyle','*'))
+        photopaths=[]
+        for i in range(len(makeups)):
+            photopaths.append(f"makeupstyle/{i+1}.jpg")
+        print(photopaths)
+
+        return redirect("/styletransfer2")
+    title = "STYLE TRANSFER"
+    return render(request,'layout.html',locals())
+    
+def styletransfer2(request):
+    if request.method =='POST' and request.POST["style"]:
+
+        beautysplit.split(request.POST["style"])
+        makeups = glob.glob(os.path.join('home','static','makeupstyle','*'))
+        photopath="./home/static/temp/split.jpg"
+        # stylephoto=os.listdir("./home/static/makeupstyle")
+        # print(stylephoto)
+        title = "SELECT THE STYLE YOU LIKE!"
+    # elif os.path('./home/static/temp/split.jpg'):
+    #     os.remove('./home/static/temp/split.jpg')  
+    
+
+    return render(request,'styletransfer2.html',locals())  
+        
+
+def objectdetection(request):
+    if request.method =='POST' and request.FILES['photoupload']:
+        myfile=request.FILES['photoupload']
+        fs = FileSystemStorage(location='home/static/images/')
+        fs.save(myfile.name,myfile)
+        # print(f'home/static/images/{myfile.name}.jpg')
+        # forImport_recognize_faces_image.readPara("home/dlib/encoding3.pickle",f'home/static/images/{myfile.name}','cnn')
+        # photopath="images/upload.jpg"
+        YoloTest.evaluate()
+        # print(YoloTest.dlist)
+        for item in YoloTest.dlist:
+            sort = Classified(**item) 
+            sort.save()
+        title="OBJECT"
+    return render(request,'layout.html',locals())
+
+
+def delmypic(request):
+    return render(request,'delmypic.html')
+
+def result(request):
+    # if request.method =='POST':
+    return render(request,'result.html',locals())
