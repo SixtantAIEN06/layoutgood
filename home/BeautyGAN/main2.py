1aeeae1a3490c9922cb2c48d03622c6922c169e4
cj <chengjie1993@hotmail.com>
2020-02-18 15:05:47 +0800

    beautyGANfinal

--- home/BeautyGAN/main2.py
+++ home/BeautyGAN/main2.py
@@ -8,6 +8,7 @@ from imageio import imread, imsave
 import cv2
 import argparse
 from PIL import Image
+import dlib
 
 
 def preprocess(img):
@@ -21,7 +22,8 @@ def beauty(image):
     image=im.convert('RGB')
     image.save('./home/static/temp/toRGB.jpg')
     org_h,org_w,_=imread('./home/static/temp/toRGB.jpg').shape
-
+    
+    predictor = dlib.shape_predictor("./home/BeautyGAN/shape_predictor_68_face_landmarks.dat")
     face_cascade = cv2.CascadeClassifier('./home/BeautyGAN/haarcascade_frontalface_default.xml')
 # Read the input image
     img = cv2.imread('./home/static/temp/toRGB.jpg')
@@ -30,14 +32,38 @@ def beauty(image):
     # Convert into grayscale
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     # Detect faces
-    faces = face_cascade.detectMultiScale(gray, 1.1, 5)
+    rects = face_cascade.detectMultiScale(gray, 1.1, 5)
     # Draw rectangle around the faces
-    face_num=1
-    for (x, y, w, h) in faces:
-    # img = cv2.imread(args.no_makeup)
+    
+    for (face_num,(x, y, w, h)) in enumerate(rects):
+        img = cv2.imread('./home/static/temp/before.jpg')
 
-    # cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 0)
-        print(x,y,w,h)
+        x_=x-w//8                       #放大臉抓取範圍
+        y_=y-h//8
+        w_=w+w//8*2
+        h_=h+h//8*2             
+        rect=dlib.rectangle(x_,y_,x_+w_,y_+h_)
+
+        dimface = np.matrix([[p.x, p.y] for p in predictor(img, rect).parts()])
+        dimface2=np.array(dimface[:17])         #下巴17個點+眉毛左右各3
+        for i in range(3):
+            # dimface[26-i]=dimface[26-i]-[-w_//10,w_//10]   #抓到眉毛以上
+            dimface2=np.append(dimface2,dimface[26-i],0)
+        for i in range(3):
+            # dimface[26-i]=dimface[19-i]-[w_//10,w_//10]
+            dimface2=np.append(dimface2,dimface[19-i],0)
+
+        face_mask_w=dimface2[np.argmax(dimface2,0)][0][0,0]-dimface2[np.argmin(dimface2,0)][0][0,0];
+        face_mask_h=dimface2[np.argmax(dimface2,0)][0][1,1]-dimface2[np.argmin(dimface2,0)][0][1,1];
+        face_mask_x=dimface2[np.argmin(dimface2,0)][0][0,0]
+        face_mask_y=dimface2[np.argmin(dimface2,0)][0][1,1]
+        center=(face_mask_x+face_mask_w//2 ,face_mask_y+face_mask_h//2)
+
+
+        mask=np.zeros((org_h,org_w,3),dtype=np.uint8)
+        cv2.fillPoly(mask,[dimface2],(255,255,255))
+        # cv2.imwrite("./home/static/temp/mask.jpg",mask)
+
         im1 = img[y:y+h,x:x+w,::-1]
 
 
@@ -54,6 +80,7 @@ def beauty(image):
         result = np.ones((img_size, (len(makeups) + 1) * img_size, 3))
         result[:img_size, :img_size] = no_makeup / 255.
         final=np.ones((org_h, (len(makeups) + 1) * org_w, 3))
+        final[:org_h, :org_w] = img 
 
         tf.reset_default_graph()
         sess = tf.Session()
@@ -85,24 +112,52 @@ def beauty(image):
         # result_=Image.open('result_.jpg')
         im_cut=[]
         final=final[:,:,::-1]
-        if face_num==1:
+        if face_num==0:
             for i in range(len(makeups)):
                 # print(i*w,0, (i+1)*w, h)
                 final[y:y+h , x+(i+1)*org_w : x+w+(i+1)*org_w]=result_[:h,(i+1)*w:(i+2)*w]
-            imsave('./home/static/temp/result.jpg', final)
+            imsave('./home/static/temp/resultsq.jpg', final)
+            for i in range(len(makeups)):
+                src=cv2.imread('./home/static/temp/resultsq.jpg')
+                src=src[:org_h,(i+1)*org_w : (i+2)*org_w]
+                org=cv2.imread('./home/static/temp/before.jpg')
+                output=cv2.seamlessClone(src, org, mask, center,  cv2.NORMAL_CLONE  )
+                print(output.shape)
+                final[:org_h,(i+1)*org_w : (i+2)*org_w]=output[:,:,::-1]
+            imsave('./home/static/temp/result.jpg', final) 
+            
         else:
             final=cv2.imread('./home/static/temp/result.jpg')[:,:,::-1]
             for i in range(len(makeups)):
                 # print(i*w,0, (i+1)*w, h)
                 final[y:y+h , x+(i+1)*org_w : x+w+(i+1)*org_w]=result_[:h,(i+1)*w:(i+2)*w]
-            imsave('./home/static/temp/result.jpg', final)        
-            
+            imsave('./home/static/temp/resultsq.jpg', final)  
+            for i in range(len(makeups)):      
+                src=cv2.imread('./home/static/temp/resultsq.jpg')
+                src=src[:org_h,(i+1)*org_w : (i+2)*org_w]
+                org=cv2.imread('./home/static/temp/result.jpg')
+                org=org[:org_h,(i+1)*org_w : (i+2)*org_w]
+
+                output=cv2.seamlessClone(src, org, mask, center,  cv2.NORMAL_CLONE  )
+                print(output.shape)
+                final[:org_h,(i+1)*org_w : (i+2)*org_w]=output[:,:,::-1]
+            imsave('./home/static/temp/result.jpg', final) 
 
         os.remove(f'./home/static/temp/im{face_num}.jpg')
 
-        face_num+=1
 
-    os.remove('./home/static/temp/result_.jpg')
+        # face_num+=1   
+        os.remove('./home/static/temp/result_.jpg')
+        os.remove('./home/static/temp/resultsq.jpg')
+
+        # for i in range(len(makeups)):
+        #     src=cv2.imread('./home/static/temp/result.jpg')[:org_h,(i+1)*org_w : (i+2)*org_w]
+        #     org=cv2.imread('./home/static/temp/before.jpg')
+        #     output=cv2.seamlessClone(src, org, mask, center,  cv2.NORMAL_CLONE  )
+        #     final[:org_h,(i+1)*org_w : (i+2)*org_w]=output[:,:,::-1]
+        #     imsave('./home/static/temp/result.jpg', final)        
+
+
     os.remove('./home/static/temp/toRGB.jpg')
-    # os.remove('./home/static/temp/before.jpg')
+
 
